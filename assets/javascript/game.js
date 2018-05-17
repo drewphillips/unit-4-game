e734585d7672c78117f8bf4e97f23e5467591754
drewphillips <drew@superheroprinting.com>
2018-05-16 20:03:21 -0700

    working code and basic HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,27 +5,33 @@ var newNumber = 0;
 
 function newGame() {
 
-    //New number
     newNumber = (Math.floor(20 + (Math.random() * 101)))
     console.log("New Game number " + newNumber)
 
     runningTotal = 0;
+    $("#runningTotal").html(runningTotal);
+    $("#newNumber").html(newNumber);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+
     for (i = 0; i < 4; i++) {
-        var crystalValue = Math.floor(Math.random() * 13)
+        var crystalValue = Math.floor(1 + (Math.random() * 12))
         $("#crystal" + i).data("crystal-number", crystalValue);
         console.log("Number " + i + " is " + crystalValue)
         $("#crystal" + i).on("click", function () {
 
             console.log("YOU CLICKED " + $(this).data("crystal-number"));
             runningTotal = runningTotal + $(this).data("crystal-number");
-
+            $("#runningTotal").html(runningTotal);
             console.log(runningTotal)
             if (runningTotal === newNumber) {
                 wins++
+                $("#wins").html(wins);
                 newGame()
             }
             else if (runningTotal > newNumber) {
                 losses++
+                $("#losses").html(losses);
                 newGame()
             }
 
